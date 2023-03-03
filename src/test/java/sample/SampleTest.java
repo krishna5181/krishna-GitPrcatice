0fb74e83381760d5b6d750667890a481d133d4aa
balar <balar@192.168.1.26>
2023-03-03 18:06:52 +0530

    SampleTest commite and push by automation lead

---
+++ src/test/java/sample/SampleTest.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+	@Test
+	public void game()
+	{
+		System.out.println("Shuttle badminton is my favourite sport");
+	}
+
+}
